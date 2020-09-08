e931bc5f314eea18bd3f24aec5a875d879c00c28
Mads <mads.v.k@hotmail.com>
2020-09-08 14:42:26 +0200

    hejsa

---
+++ Private projekter/WebshopSQLServer/WebshopSQLServer/WebshopSQLServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TECHCOOL;
+namespace WebshopSQLServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLet.ConnectSqlServer("Webshop", @"MADS-V-KLAUSEN\MSSQLSERVER01");
+
+            showBasket();
+        }
+
+        static void showBasket()
+        {
+            string[][] result = SQLet.GetArray("SELECT * FROM Basket");
+            foreach (var row in result)
+            {
+                foreach (var field in row)
+                {
+                    Console.Write(field + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
