9f33d4086e64199af01a06d7d04135a1629704cf
Mads <mads.v.k@hotmail.com>
2020-09-23 10:47:50 +0200

    LagerSystem oprettelse

---
+++ H1-Grundlæggende programering/LagerSystemH1/LagersystemH1/LagersystemH1/Database.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagersystemH1
+{
+    class Database
+    {
+
+    }
+}
---
+++ H1-Grundlæggende programering/LagerSystemH1/LagersystemH1/LagersystemH1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace LagersystemH1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder builder =
+            new SqlConnectionStringBuilder();
+
+            builder.DataSource = @"MADS - V - KLAUSEN\MSSQLSERVER";
+            builder.InitialCatalog = "Lager(H1)";
+            builder.IntegratedSecurity = true;
+
+            SqlConnection Connection = new SqlConnection(builder.ConnectionString);
+        }
+    }
+}
