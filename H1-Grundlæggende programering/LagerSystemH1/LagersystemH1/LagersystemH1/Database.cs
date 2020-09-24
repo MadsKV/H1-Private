1d56e5044d5d851dd4fa6cf6f64fe5e433bb6d31
Mads <mads.v.k@hotmail.com>
2020-09-24 13:03:58 +0200

    sql statements

--- H1-Grundlæggende programering/LagerSystemH1/LagersystemH1/LagersystemH1/Database.cs
+++ H1-Grundlæggende programering/LagerSystemH1/LagersystemH1/LagersystemH1/Database.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
 using System.Net.Sockets;
+using System.Xml.XPath;
 
 namespace LagersystemH1
 {
@@ -15,7 +16,6 @@ namespace LagersystemH1
         INSERT INTO Customers (First_Name, Last_Name, Address, Zip_Code, City)
         VALUES ('{0}', '{1}', '{2}', {3}, '{4}')";
         string formatted = string.Format(sql, firstName, lastName, address, zip, city);
-        //Console.WriteLine(formatted);
         SqlCommand command = new SqlCommand(formatted, connection);
         }
         //Insert a new Item into the database.
@@ -25,7 +25,6 @@ namespace LagersystemH1
         INSERT INTO Items (Item_Name, Item,Quantity, Item_Price, Customer_ID, Item_Order_Date)
         VALUES ('{0}', {1}, {2}, {3}, '{4}')";
             string formatted = string.Format(sql, itemName, itemQuantity, itemPrice, customerId, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-            //Console.WriteLine(formatted);
             SqlCommand command = new SqlCommand(formatted, connection);
         }
         //Delete a specific Customer with the Customer_ID
@@ -34,7 +33,6 @@ namespace LagersystemH1
             string sql = @"
             DELETE FROM Customers WHERE Customer_ID = {0}";
             string formatted = string.Format(sql, ID);
-            //Console.WriteLine(formatted);
             SqlCommand command = new SqlCommand(formatted, connection);
         }
         //Delete a specific item with the Item_ID
@@ -43,7 +41,6 @@ namespace LagersystemH1
             string sql = @"
             DELETE FROM Customers WHERE Item_ID = {0}";
             string formatted = string.Format(sql, ID);
-            //Console.WriteLine(formatted);
             SqlCommand command = new SqlCommand(formatted, connection);
         }
         //Updates a current customers information
@@ -52,16 +49,33 @@ namespace LagersystemH1
             string sql = @"
             UPDATE Customer SET First_Name = '{1}', Last_Name = '{2}', Address = '{3}', Zip_Code = {4}, City = '{5}' WHERE Customer_ID = {0} ";
             string formatted = string.Format(sql, ID, firstName, lastName, address, zip, city);
-            //Console.WriteLine(formatted);
             SqlCommand command = new SqlCommand(formatted, connection);
         }
-        //Updates a current customers information
-        public static void updateItem(string ID, string Message, SqlConnection connection)
+        //Updates a current items information
+        public static void updateItem(string ID, string itemName, int itemQuantity, double itemPrice, int customerID, string itemOrderDate, SqlConnection connection)
         {
             string sql = @"
-            UPDATE Items SET First_Name = '{1}', Last_Name = '{2}', Address = '{3}', Zip_Code = {4}, City = '{5}' WHERE Customer_ID = {0} ";
-            string formatted = string.Format(sql, ID, firstName, lastName, address, zip, city);
-            //Console.WriteLine(formatted);
+            UPDATE Items SET Item_Name = '{1}', Item_Quantity = {2}, Item_Price = {3}, Customer_ID = '{4}', Item_Order_Date = {5} WHERE Item_ID = {0} ";
+            string formatted = string.Format(sql, ID, itemName, itemQuantity, itemPrice, customerID, itemOrderDate);
+            SqlCommand command = new SqlCommand(formatted, connection);
+        }
+        //Displays a list of all current items in stock.
+        public static void SelectAllItems(SqlConnection connection)
+        {
+            string sql = @"
+            SELECT * FROM Items
+            ORDER BY Item_ID DESC";
+            string formatted = string.Format(sql);
+            SqlCommand command = new SqlCommand(formatted, connection);
+            
+        }
+        //Displays a list of all current customers.
+        public static void SelectAllCustomers(SqlConnection connection)
+        {
+            string sql = @"
+            SELECT * FROM Customers
+            ORDER BY Customer_ID DESC";
+            string formatted = string.Format(sql);
             SqlCommand command = new SqlCommand(formatted, connection);
         }
     }
--- H1-Grundlæggende programering/LagerSystemH1/LagersystemH1/LagersystemH1/Program.cs
+++ H1-Grundlæggende programering/LagerSystemH1/LagersystemH1/LagersystemH1/Program.cs
@@ -16,12 +16,189 @@ namespace LagersystemH1
             SqlConnection connection = new SqlConnection(builder.ConnectionString);
 
 
-            string sqlInsert = @"INSERT INTO Customers (First_Name, Last_Name, Address, Zip_Code, City)
+            /*string sqlInsert = @"INSERT INTO Customers (First_Name, Last_Name, Address, Zip_Code, City)
             VALUES ('Mads', 'Klausen', 'Ditlev Bergs vej 71,3,9', 9000, 'Aalborg')";
             connection.Open();
             SqlCommand command = new SqlCommand(sqlInsert, connection);
-            command.ExecuteNonQuery();
+            command.ExecuteNonQuery();*/
 
+            const ConsoleKey keyInfo1 = ConsoleKey.D1;
+            const ConsoleKey keyInfo2 = ConsoleKey.D2;
+            const ConsoleKey keyInfo3 = ConsoleKey.D3;
+            const ConsoleKey keyInfo4 = ConsoleKey.D4;
+            const ConsoleKey keyInfo5 = ConsoleKey.D5;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor
+                = ConsoleColor.DarkGray;
+                Console.WriteLine("" +
+                    "Press 1 :For Customer Management.\n" +
+                    "Press 2 :For Item Management.\n" +
+                    "Press 3 :To EXIT.\n");
+
+                ConsoleKey pressedKey = PressedKey();
+
+                switch (pressedKey)
+                {
+                    case keyInfo1:
+                        Console.ForegroundColor
+                        = ConsoleColor.Blue;
+
+                        switch (pressedKey)
+                        {
+                            case keyInfo1:
+                                Console.ForegroundColor
+                                = ConsoleColor.Blue;
+                                Console.Clear();
+                                Console.WriteLine("" +
+                                "Press 1 :To Create a new Customer account.\n" +
+                                "Press 2 :To Delete a current Customer account.\n" +
+                                "Press 3 :To Update a current Customer account.\n" +
+                                "Press 4 :To View the current list of Customers.\n" +
+                                "Press 5 :To EXIT.\n");
+
+                                pressedKey = PressedKey();
+
+                                switch (pressedKey)
+                                {
+                                    case keyInfo1:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        Console.WriteLine("Enter the credentials of the customer in this format : \n\n" +
+                                            ">First name, Last name, Address, Zip Code and their City<");
+                                        string sqlInsert = @"INSERT INTO Customers (First_Name, Last_Name, Address, Zip_Code, City)
+                                        VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')";
+                                        connection.Open();
+                                        SqlCommand command = new SqlCommand(sqlInsert, connection);
+                                        command.ExecuteNonQuery();
+                                        break;
+
+                                    case keyInfo2:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        break;
+
+                                    case keyInfo3:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        break;
+
+                                    case keyInfo4:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        break;
+
+                                    case keyInfo5:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        break;
+                                }
+                                /*Console.Write("Create New Customer > ");
+                                string input = Console.ReadLine();
+                                Console.Write("Enter the customer credentials > ");*/
+                                string firstName = (Console.ReadLine());
+                                string lastName = (Console.ReadLine());
+                                string address = (Console.ReadLine());
+                                int zip = (Console.Read());
+                                string city = (Console.ReadLine());
+
+                                SqlCommand customercommand = new SqlCommand();
+                                customercommand.ExecuteNonQuery();
+                                //Database.insertCusotmer(firstName, lastName, address, zip, city);
+                                break;
+                        }
+                        /*Console.Write("Enter new message > ");
+                        string input = Console.ReadLine();
+                        Console.Write("What author ID is this message connected to? > ");
+
+                        int messagesauthor = int.Parse(Console.ReadLine());
+                        Database.insertMessage(input, messagesauthor);*/
+                        break;
+
+                    case keyInfo2:
+                        Console.ForegroundColor
+                        = ConsoleColor.Blue;
+                        Console.Clear();
+                        Console.WriteLine("" +
+                        "Press 1 :To Create a new Item.\n" +
+                        "Press 2 :To Delete a current Item in stock.\n" +
+                        "Press 3 :To Update a current Item in stock.\n" +
+                        "Press 4 :To View the current list of Items in stock.\n" +
+                        "Press 5 :To EXIT.\n");
+
+                        pressedKey = PressedKey();
+
+                        switch (pressedKey)
+                        {
+                            case keyInfo1:
+
+                                switch (pressedKey)
+                                {
+                                    case keyInfo1:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        Console.WriteLine("Enter the Information about the item in this format : \n\n" +
+                                            ">Item name, Item quantity, Item price, Item Order date<");
+                                        string sqlInsert = @"INSERT INTO Items (Item_Name, Item_Quantity, Item,Price, Item_Order_ID)
+                                        VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')";
+                                        connection.Open();
+                                        SqlCommand command = new SqlCommand(sqlInsert, connection);
+                                        command.ExecuteNonQuery();
+                                        break;
+
+                                    case keyInfo2:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        break;
+
+                                    case keyInfo3:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        break;
+
+                                    case keyInfo4:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        break;
+
+                                    case keyInfo5:
+                                        Console.ForegroundColor
+                                        = ConsoleColor.Blue;
+                                        break;
+                                }
+                                /*Console.Write("Create New Customer > ");
+                                string input = Console.ReadLine();
+                                Console.Write("Enter the customer credentials > ");*/
+                                string firstName = (Console.ReadLine());
+                                string lastName = (Console.ReadLine());
+                                string address = (Console.ReadLine());
+                                int zip = (Console.Read());
+                                string city = (Console.ReadLine());
+
+                                SqlCommand customercommand = new SqlCommand();
+                                customercommand.ExecuteNonQuery();
+                                //Database.insertCusotmer(firstName, lastName, address, zip, city);
+                                break;
+                        }
+                        break;
+
+                    case keyInfo3:
+                        break;
+                }
+            }
+            ConsoleKey PressedKey()
+            {
+                do
+                {
+                    while (!Console.KeyAvailable) ;
+                    ConsoleKey pressed = Console.ReadKey(true).Key;
+                    if (pressed == keyInfo1 || pressed == keyInfo2 || pressed == keyInfo3 || pressed == keyInfo4 || pressed == keyInfo5)
+                        return pressed;
+                    Console.Clear();
+                } while (true);
+            }
         }
     }
 }
