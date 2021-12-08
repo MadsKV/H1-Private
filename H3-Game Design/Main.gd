2f8e543d6cbc165e6839490577da2aa316e4397c
MadsKV <mads.v.k@hotmail.com>
2021-12-08 14:20:28 +0100

    game

--- H3-Game Design/Main.gd
+++ H3-Game Design/Main.gd
@@ -1,18 +1,34 @@
-extends RigidBody2D
-export var min_speed = 150  # Minimum speed range.
-export var max_speed = 250  # Maximum speed range.
-
+extends Node
 var mob = preload("res://Mobs.tscn")
+var arrow = load("res://Crosshair/cross_normal.png")
+
+var currentWave = 0;
+var left = 0;
+
+var waves  = [5, 10, 15];
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var m = mob.instance()
-	add_child(m)
-	m.position = Vector2(100, 100)
+	
+	randomize()
 	pass # Replace with function body.
+	
+	# Replaces the cursor with a crosshair.
+	Input.set_custom_mouse_cursor(arrow)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-
+	if left == 0:
+		left = waves [currentWave]
+		currentWave += 1
+		spawnEnemies();
 	pass
+	
+func spawnEnemies():
+	for i in range(left):
+		var m = mob.instance()
+		add_child(m)
+		m.position = Vector2(rand_range(-1000, -100), rand_range(100, 800))
+	
